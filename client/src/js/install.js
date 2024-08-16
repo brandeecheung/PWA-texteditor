046c919947f972882e71f7afc2f5eea1e1ae690d
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-08-15 20:46:48 -0500

    edit install js for install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Variable to hold the beforeinstallprompt event
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Show the install button when the beforeinstallprompt event is fired
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+  butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implement a click event handler on the butInstall element
+butInstall.addEventListener('click', async () => {
+  // Hide the install button
+  butInstall.style.display = 'none';
+  // Show the install prompt
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    // Clear the deferredPrompt variable
+    deferredPrompt = null;
+  }
+});
+
+// Add a handler for the appinstalled event
+window.addEventListener('appinstalled', () => {
+    console.log('PWA was installed');
+  });
+  
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,7 +18,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new GenerateSW(), // Generates a service worker with Workbox
+
+
+      // WebpackPwaManifest plugin to generate a PWA manifest file
+      new WebpackPwaManifest({
+        name: 'JATE App', 
+        short_name: 'JATE', 
+        description: 'Just Another Text Editor', 
+        background_color: '#ffffff', 
+        theme_color: '#000000',
+        start_url: '/', 
+        publicPath: '/', 
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'), 
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assets', 'icons'), 
+          },
+        ],
+      }),
     ],
 
     module: {
